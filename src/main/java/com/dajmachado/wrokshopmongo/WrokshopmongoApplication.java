ca8cf405c5b916806b997869b017899952679a93
Daniel Machado <dajmachado@gmail.com>
2023-02-09 12:28:01 -0300

    primeiro commit

---
+++ src/main/java/com/dajmachado/wrokshopmongo/WrokshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.dajmachado.wrokshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrokshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrokshopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dajmachado/wrokshopmongo/WrokshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dajmachado.wrokshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrokshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
