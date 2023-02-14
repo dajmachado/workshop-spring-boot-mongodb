a4e3b299d773bdcab7f85021c2cba63e05046522
Daniel Machado <dajmachado@gmail.com>
2023-02-14 13:26:23 -0300

    atualização de usuário com PUT

--- src/main/java/com/dajmachado/wrokshopmongo/services/UserServices.java
+++ src/main/java/com/dajmachado/wrokshopmongo/services/UserServices.java
@@ -1,6 +1,5 @@
 package com.dajmachado.wrokshopmongo.services;
 
-import java.security.PublicKey;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,28 +13,40 @@ import com.dajmachado.wrokshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserServices {
-
+	
 	@Autowired
 	private UserRepository repo;
 
-	public List<User> findAll() {
-
+	public List<User> findAll(){
+		
 		return repo.findAll();
-
+		
 	}
-
-	public User findById(String id) {
+	
+	public User findById(String id){
 		Optional<User> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+			return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-
+	
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
 
+
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
+		}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());	
 	}
 
 	public User fromDTO(UserDTO objDTO) {
